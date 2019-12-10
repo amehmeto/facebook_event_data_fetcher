cf8ddc114f55c8fdc44781614e8d8e1edf4298d6
Arthur <42amehmeto@gmail.com>
2019-12-10 08:30:59 +0100

    :alembic: Experimenting with graph.facebook.com/{user_id}/events

--- fbevent.js
+++ fbevent.js
@@ -3,14 +3,24 @@
 const shellExec = require('shell-exec');
 
 const USER_ID = '3258104107540071';
-const ACCESS_TOKEN = 'EAAGLakOhBHsBANeXasflWeTqHLWU7exnktSi1jrCDb4DSy6HlCdn0UxKB8l7JAFaeFsTqH0huV4RGa6OcXNZCZCULok2zh2fpU5TWleKEB77itqKgTgOSbLm0XU2JBUlgt5Bs9TtlgsWE3tuvJ6f2fzVS7CMzfEuOh3Suebg9WAQ3n9QqJNQWdisAb47wZD';
+const FIELDS = 'events';
+const ACCESS_TOKEN = 'EAAGLakOhBHsBAE44W3elZAcwUgHEMORSzhQ6vatAvvrtBHktZCBCdmTnuEyss1cRjifR6QWb7AZAe5uMaFTQNdMdDExtvkvyi5qHu6LRr89SmbVkbeJqsdLnDXHv1yAjPCZCRh7N15nHNReDhQIiN6ER1ToL5dfuvXDvQhiZBcTgqc8QkJy4A7knI5RvidLUZD';
 
 shellExec(
     "curl \"https://graph.facebook.com/" +
     USER_ID +
-    "?fields=id,name&access_token=" +
+    "/events" +
+    "?" +
+    //"fields=" +
+    //FIELDS +
+    "limit=999" +
+    "&" +
+    "access_token=" +
     ACCESS_TOKEN +
     "\""
 ).then((stdout) => {
-    console.log(stdout.stdout);
+    console.log(
+        //JSON.stringify(stdout.stdout, null, 4)
+        stdout.stdout
+);
 });
