b77691cfaa700d0342c3c8fbddd8e59d422063cc
Arthur <42amehmeto@gmail.com>
2019-12-10 10:49:20 +0100

    :recycle: Extracting till drop

--- fbevent.js
+++ fbevent.js
@@ -6,29 +6,41 @@ const USER_ID = '3258104107540071';
 const FIELDS = 'end_time';
 const ACCESS_TOKEN = 'EAAGLakOhBHsBAHK7eniZBRSnOyiJkdkI4VYC0sEwxGmdRbYiIHhXZAwcCrqRyMfs6YaqNXKVCknr10IpsPXspg2ygZAyNdoROqYPZBF0AG7eOYffhaqItTgAB8hrx1GZCUA2wsoW0ZBRYBL3HtDLu5PJOyAPnhwiwXrPkqe13m3ZCS75xOcIKZA3dCbQdcTJAVpnyH1y7UeU3UgnPZCLkaCx1DwBHWQe3YDwZD';
 
-shellExec(
-    "curl \"https://graph.facebook.com/" +
+const CURL_REQUEST = "curl \"https://graph.facebook.com/" +
     USER_ID +
     "/events" +
     "?" +
     //"fields=" +
     //FIELDS +
-    //"limit=999" +
-    //"&" +
+    "limit=100" +
+    "&" +
     "access_token=" +
     ACCESS_TOKEN +
-    "\""
-).then((stdout) => {
-    const RAW_EVENT_DATA = JSON.parse(stdout.stdout);
+    "\"";
 
-    const WANTED_EVENT_DATA = {
-        "event_name": RAW_EVENT_DATA.data,
+function parse(shellOutput) {
+    return JSON.parse(shellOutput).data[0];
+}
+
+function rearrangeDataAsPerWantedFormat(RAW_EVENT_DATA) {
+    return {
+        "event_name": RAW_EVENT_DATA.name,
         "start_time": RAW_EVENT_DATA.start_time,
         "end_time": RAW_EVENT_DATA.end_time,
         "cover": RAW_EVENT_DATA.cover,
         "days_before_token_expiration": RAW_EVENT_DATA.days_before_token_expiration,
     };
-    console.log(WANTED_EVENT_DATA);
-    console.log("\n");
+}
+
+function displayEventData(RAW_EVENT_DATA, WANTED_EVENT_DATA) {
     console.log(RAW_EVENT_DATA);
+    console.log("\n");
+    console.log(WANTED_EVENT_DATA);
+}
+
+shellExec(CURL_REQUEST).then((shellOutput) => {
+    const RAW_EVENT_DATA = parse(shellOutput.stdout);
+    const WANTED_EVENT_DATA = rearrangeDataAsPerWantedFormat(RAW_EVENT_DATA);
+
+    displayEventData(RAW_EVENT_DATA, WANTED_EVENT_DATA);
 });
