e0795d5763f5c071eb34e2998ee1ea62b7f85567
Arthur <42amehmeto@gmail.com>
2019-12-10 11:30:29 +0100

    :sparkles: Converting date to dd/mm/yyyy hh:mm format

--- fbevent.js
+++ fbevent.js
@@ -3,30 +3,27 @@
 const shellExec = require('shell-exec');
 
 const USER_ID = '3258104107540071';
-const FIELDS = 'end_time';
 const ACCESS_TOKEN = 'EAAGLakOhBHsBAHK7eniZBRSnOyiJkdkI4VYC0sEwxGmdRbYiIHhXZAwcCrqRyMfs6YaqNXKVCknr10IpsPXspg2ygZAyNdoROqYPZBF0AG7eOYffhaqItTgAB8hrx1GZCUA2wsoW0ZBRYBL3HtDLu5PJOyAPnhwiwXrPkqe13m3ZCS75xOcIKZA3dCbQdcTJAVpnyH1y7UeU3UgnPZCLkaCx1DwBHWQe3YDwZD';
 
-const CURL_REQUEST = "curl \"https://graph.facebook.com/" +
-    USER_ID +
-    "/events" +
-    "?" +
-    //"fields=" +
-    //FIELDS +
-    "limit=100" +
-    "&" +
-    "access_token=" +
-    ACCESS_TOKEN +
-    "\"";
+const CURL_REQUEST = "curl \"https://graph.facebook.com/" + USER_ID + "/events" +
+    "?" + "access_token=" + ACCESS_TOKEN + "\"";
 
 function parse(shellOutput) {
     return JSON.parse(shellOutput).data[0];
 }
 
+function convertToReadableFormat(time) {
+    const FACEBOOK_DATE_PATTERN = /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2})/;
+    const DATE = time.match(FACEBOOK_DATE_PATTERN);
+
+    return DATE[3] + '/' + DATE[2] + '/' + DATE[1] + ' ' + DATE[4] + ':' + DATE[5] ;
+}
+
 function rearrangeDataAsPerWantedFormat(RAW_EVENT_DATA) {
     return {
         "event_name": RAW_EVENT_DATA.name,
-        "start_time": RAW_EVENT_DATA.start_time,
-        "end_time": RAW_EVENT_DATA.end_time,
+        "start_time": convertToReadableFormat(RAW_EVENT_DATA.start_time),
+        "end_time": convertToReadableFormat(RAW_EVENT_DATA.end_time),
         "cover": RAW_EVENT_DATA.cover,
         "days_before_token_expiration": RAW_EVENT_DATA.days_before_token_expiration,
     };
